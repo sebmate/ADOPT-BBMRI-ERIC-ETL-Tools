34571ad25475d9a12df973a3cd1d21391f60ee45
Sebastian Mate <sebastian.mate@fau.de>
2018-04-19 14:06:32 +0200

    Moved ETLHelper directory to correct position.

--- ETLHelper/src/de/fau/med/imi/MDRPipe/ETLHelper/XMLEntry.java
+++
@@ -1,5 +0,0 @@
-package de.fau.med.imi.MDRPipe.ETLHelper;
-
-public interface XMLEntry {
-
-}
--- ETLHelper/src/de/samply/schema/store/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2018.04.03 um 08:58:56 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schema.samply.de/store", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.samply.schema.store;
