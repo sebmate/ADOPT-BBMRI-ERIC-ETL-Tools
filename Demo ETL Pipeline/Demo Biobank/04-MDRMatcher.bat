84670f8be2b86bb8d0bdd7c8857b1328ba7f54e5
Sebastian Mate <cyberseb@mailbox.org>
2018-10-11 08:45:39 +0200

    Tweaks to the batch file

--- Demo ETL Pipeline/Demo Biobank/04-MDRMatcher.bat
+++ Demo ETL Pipeline/Demo Biobank/04-MDRMatcher.bat
@@ -1,2 +1,9 @@
-java -jar ../MDRMatcher.jar -t metadata\ccdg.tsv -s metadata\local.tsv -o mappings\local-ccdg.tsv
+@echo off
+
+@echo Started: %date% %time%
+
+java -XX:-UseGCOverheadLimit -jar ../MDRMatcher.jar -t metadata\ccdg.tsv -s metadata\local.tsv -o mappings\local-ccdg.tsv
+
+@echo Finished: %date% %time%
+
 pause
