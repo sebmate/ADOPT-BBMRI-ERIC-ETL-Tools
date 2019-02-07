85bc906a752a16bc992134bb89f13ae9dd1ca86b
Sebastian Mate <cyberseb@mailbox.org>
2019-02-07 12:05:37 +0100

    Delete 04-MDRMatcher.bat

--- Demo ETL Pipeline/Demo Biobank/04-MDRMatcher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-@echo Started: %date% %time%
-
-java -XX:-UseGCOverheadLimit -jar ../MDRMatcher.jar -t metadata\ccdg.tsv -s metadata\local.tsv -o mappings\local-ccdg.tsv
-
-@echo Finished: %date% %time%
-
-pause
